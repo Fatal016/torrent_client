a332b518b1dfe1a67a1b315dd534d8f5e3ad90cf
Fatal016 <016erw@gmail.com>
2023-10-17 14:10:36 -0400

    Bencode structure down, working on implementing state machine

--- bencode_parser.c
+++ bencode_parser.c
@@ -1,5 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "./bencode_parser.h"
 
 int main() {
+	struct bencode_module bencode;
+	enum parse_state state = announce;;
+
+	char id;
+	char readBuffer[128];
+	char readBufferIndex;
+	
+	int stringLength;
+
+
+	/* Accessing .torrent file in read-only mode */	
+	FILE *file = fopen("test.torrent", "r");
+	if (file == NULL) {
+		perror("Error opening file");
+		return -1;
+	}
+
+
+	/* Checking first character for start of dictionary */
+	id = fgetc(file);
+	if (id != 'd') {
+		printf("Improper Bencode format: First char is not dictionary");
+		exit(-1);
+	}
+
+	for (readBufferIndex = 0; readBufferIndex < sizeof(readBuffer) / sizeof(char); readBufferIndex++) {
+		id = fgetc(file);
+		if (id != 58) {
+			readBuffer[readBufferIndex] = id;
+			continue;
+		}
+		break;
+	}
+	printf("Buffer Length: %d\n", readBufferIndex);
+	
+	stringLength = atoi(readBuffer);
+	printf("String Length: %d\n", stringLength);
+	
+	memset(readBuffer, 0, sizeof(readBuffer));
+	for (int i = 0; i < stringLength; i++) {
+		readBuffer[i] = fgetc(file);
+	}
+
+	printf("%s\n", readBuffer);
+
+	if (strcmp(readBuffer, "announce") == 0) {
+		
+		bencode.announce = readBuffer;
+		printf("Length of announce: %ld\n", (sizeof(bencode.announce) / sizeof(char)));
+		//printf("Found announcement\n");
+//	} else if (strcmp(readBuffer, ))
+	}
+	fclose(file);
+	exit(0);
 }
--- bencode_parser.h
+++ bencode_parser.h
@@ -1,11 +1,24 @@
-struct BencodeValue {
-	enum { BENCODE_INTEGER, BENCODE_STRING, BENCODE_LIST, BENCODE_DICT } type;
-	union {
-		long long integer;
-		char* string;
-		struct BencodeValue* list;
-		struct BencodeDictPair *dict;
-	} data;
+enum parse_state { announce, announce_list, comment, created_by, creation_date, bencode_info, url_list };
+
+struct info_file {
+	char* length;
+	char* path;
 };
 
-struct BencodeValue* parse_bencode(const char* input);
+struct bencode_info {
+	struct info_file* files;
+	char* name;
+	char* piece_length;
+	char* pieces;
+};
+
+struct bencode_module {
+	char* announce;
+	char* announce_list;
+	char* comment;
+	char* created_by;
+	char* creation_date;
+	char* encoding;
+	struct bencode_info info;
+	char** url_list;
+};
