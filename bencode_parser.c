7c0df951f0da58f4f3217a37418d4b6d7ca00396
Ethan Woods <ethan.woods@maine.edu>
2023-11-06 23:05:41 -0500

    Desktop update

--- bencode_parser.c
+++ bencode_parser.c
@@ -5,19 +5,18 @@
 #include "./bencode_parser.h"
 
 int main() {
-	enum PARSE_ENUM { FOREACH_STATE(GENERATE_ENUM) };
-	static const char *PARSE_STATE[] = { FOREACH_STATE(GENERATE_STRING) };
-
 	struct bencode_module bencode;
+	
 	enum PARSE_ENUM state = announce;
+	static const char *parse_state[] = { FOREACH_STATE(GENERATE_STRING) };
 
 	int charIterator;
-	char id;
+	char charIn;
 	char readBuffer[128];
 	char readBufferIndex;
 	
 	int stringLength;
-
+	int result;
 
 	/* Accessing .torrent file in read-only mode */	
 	FILE *file = fopen("test.torrent", "r");
@@ -26,41 +25,58 @@ int main() {
 		return -1;
 	}
 
-
 	/* Checking first character for start of dictionary */
-	id = fgetc(file);
-	if (id != 'd') {
-		printf("Improper Bencode format: First char is not dictionary");
-		exit(-1);
+	charIn = fgetc(file);
+
+	/* Error checking for if file contents are not standard Bencode layout */
+	if (charIn != 'd') {
+		printf("Improper Bencode Format: First character is not start of dictionary\n");
+		return -1;
 	}
 
-	for (readBufferIndex = 0; readBufferIndex < sizeof(readBuffer) / sizeof(char); readBufferIndex++) {
-		id = fgetc(file);
-		if (id != 58) {
-			readBuffer[readBufferIndex] = id;
-			continue;
-		}
-		break;
-	}
+	/* Beginning root dictionary parse */
+	result = pdict(charIn, readBuffer, readBufferIndex, state, parse_state, stringLength, bencode, file);
 	
-	pstr(readBuffer, readBufferIndex, stringLength, file);
-	printf("%s\n", readBuffer);
-
-	if (strcmp(readBuffer, PARSE_STATE[state]) == 0) {		
-		bencode.announce = readBuffer;
-		printf("Length of announce: %ld\n", (sizeof(bencode.announce) / sizeof(char)));
-	} else {
-		state++;
+	/* Error handling for root dictionary parse */
+	if (result == -1) {
+		printf("Error encountered while parsing dictionary\n");
+		return -1;
 	}
+
 	fclose(file);
-	exit(0);
+	return 0;
 }
 
 /* (p)arse (str)ing */
-void pstr(char* readBuffer, int readBufferIndex, int stringLength, FILE* file) {
-    stringLength = atoi(readBuffer);
-	memset(readBuffer, 0, sizeof(readBuffer));
+int pstr(char* readBuffer, int readBufferIndex, int stringLength, FILE* file) {
+    stringLength = atoi(readBuffer); // Converting parsed data-segment length into int
+	memset(readBuffer, 0, sizeof(readBuffer)); // Clearing readBuffer index in case there's remnants of previous segment
     for (readBufferIndex = 0; readBufferIndex < stringLength; readBufferIndex++) {
 	    readBuffer[readBufferIndex] = fgetc(file);
     }
+}
+
+/* Bencode dictionary parser */
+int pdict(char charIn, char* readBuffer, int readBufferIndex, enum PARSE_ENUM state, const char *parse_state[], int stringLength, struct bencode_module bencode, FILE* file) {
+	while (charIn != EOF) {
+		for (readBufferIndex = 0; readBufferIndex < sizeof(readBuffer) / sizeof(char); readBufferIndex++) {
+			charIn = fgetc(file);
+
+
+			if (charIn != 58) { // Colon is delimiter for separation between data-type and data
+				readBuffer[readBufferIndex] = charIn;
+				continue;
+			}
+			break;
+		}
+		pstr(readBuffer, readBufferIndex, stringLength, file);
+		printf("%s\n", readBuffer);	
+		
+		if (strcmp(readBuffer, parse_state[state]) == 0) {		
+			bencode.announce = readBuffer;
+			printf("Length of announce: %ld\n", (sizeof(bencode.announce) / sizeof(char)));
+		} else {
+			state++;
+		}
+	}
 }
--- bencode_parser.h
+++ bencode_parser.h
@@ -1,4 +1,16 @@
-enum parse_state { announce, announce_list, comment, created_by, creation_date, bencode_info, url_list };
+#define FOREACH_STATE(STATE) \
+		STATE(announce)			\
+		STATE(announce_list)	\
+		STATE(comment)			\
+		STATE(created_by)		\
+		STATE(creation_date)	\
+		STATE(bencode_info)		\
+		STATE(url_list)			\
+
+#define GENERATE_ENUM(ENUM) ENUM,
+#define GENERATE_STRING(STRING) #STRING,
+
+enum PARSE_ENUM { FOREACH_STATE(GENERATE_ENUM) };
 
 struct info_file {
 	char* length;
@@ -23,17 +35,5 @@ struct bencode_module {
 	char** url_list;
 };
 
-#define FOREACH_STATE(STATE) \
-		STATE(announce)			\
-		STATE(announce_list)	\
-		STATE(comment)			\
-		STATE(created_by)		\
-		STATE(creation_date)	\
-		STATE(bencode_info)		\
-		STATE(url_list)			\
-
-#define GENERATE_ENUM(ENUM) ENUM,
-#define GENERATE_STRING(STRING) #STRING,
-
-void pstr(char* readBuffer, int readBufferIndex, int stringLength, FILE* file);
-void pdict(char* readBuffer, int readBufferIndex, PARSE_ENUM state, const char *parse_state[], int stringLength, struct bencode_module bencode, FILE* file);
+int pstr(char* readBuffer, int readBufferIndex, int stringLength, FILE* file);
+int pdict(char charIn, char* readBuffer, int readBufferIndex, enum PARSE_ENUM state, const char *parse_state[], int stringLength, struct bencode_module bencode, FILE* file);
